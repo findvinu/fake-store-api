dad08a1340afccc21f672db563c6b7493c45b66b
findvinu <vinodyadavhhh4@gmail.com>
2024-04-23 11:06:55 +0530

    remove setting menu

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -15,15 +15,9 @@ import { NavLink } from "react-router-dom";
 import MyPic from "../../assets/images/my_pic.jpg";
 import Logo from "../../assets/images/vinuBytes.png";
 
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
-
 function Header() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
@@ -92,33 +86,11 @@ function Header() {
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+            <Tooltip>
+              <IconButton sx={{ p: 0 }}>
                 <Avatar alt="Remy Sharp" src={MyPic} />
               </IconButton>
             </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
           </Box>
         </Toolbar>
       </Container>
